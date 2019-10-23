269639e7b6d07219a9581eea5486e7847399e005
viktor <viktor.lekic95@gmail.com>
2019-10-23 09:35:54 +0200

    getting role information, needs rework

--- src/app/authenticated/profile/profile.component.ts
+++ src/app/authenticated/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-profile',
@@ -10,7 +11,19 @@ export class ProfileComponent implements OnInit {
 
   constructor(public auth: AuthService) { }
 
+  private email;
+
   ngOnInit() {
+    this.kebab();
   }
 
+  kebab() {
+    var _url = "http://localhost:4200/roles";
+    this.auth.getUser$().subscribe(
+      res => {
+        let role = res[_url];
+        console.log(role)
+      }
+    )
+  }
 }
